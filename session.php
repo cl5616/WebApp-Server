538cecf52e34c1c3634247588765d5392a360779
2019 <434791602@qq.com>
2018-06-14 00:32:18 +0100

    session enable

--- session.php
+++ session.php
@@ -3,10 +3,10 @@
 require_once "utils.php";
 function getCurUserId()
 {
-    return 2;
-    /*if (!isset($_SESSION["id"]))
+    //return 2;
+    if (!isset($_SESSION["id"]))
     {
         dieWithErrorMsg("not login yet");
     }
-    return $_SESSION["id"];*/
+    return $_SESSION["id"];
 }
